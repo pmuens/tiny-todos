015173b36d20c39d6e104eded9179c3a8cf0aea5
Philipp Muens <raute1337@gmx.de>
2012-06-09 12:29:48 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TinyTodos::Application.config.secret_token = 'e19bce34b862bc537da47f36ffa756311c53d07a4647650c4589fddbec2d4c4d5e2afd309c810ff01a7329a24d9a1884ae8a54967512598f751fb526eea30334'
